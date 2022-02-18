6db582bb8f7c86c6357b99b44ffa1f77841376ea
Jason Jōb <jason.allan.job@gmail.com>
2022-02-17 21:03:45 -0800

    added etherscan link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import '../App.scss';
 import GitHub from '../img/github.svg';
+import Etherscan from '../img/etherscan-logo-circle.svg';
 
 function Footer(props) {
   return (
@@ -9,6 +10,17 @@ function Footer(props) {
           <img
             alt="GitHub Logo"
             src={GitHub}
+            style={{
+              width: 36,
+              margin: '20px 10px',
+              display: 'inline-block',
+            }}
+          />
+        </a>
+        <a href="https://etherscan.io/address/0x1Fc3e820AA7368f9Dde7B84758Dd208123159Ad6">
+          <img
+            alt="Etherscan Logo"
+            src={Etherscan}
             style={{
               width: 36,
               margin: '20px 10px',
