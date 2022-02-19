244d4f1bf062c8de5a4cee836057ea830b4031f9
Jason Jōb <jason.allan.job@gmail.com>
2022-02-19 10:37:27 -0800

    added gas info

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,6 +7,7 @@ function Info(props) {
       <p style={{ fontSize: '1.8rem', marginBottom: 10 }}>
         <b>Claim a star system</b>
       </p>
+      <p style={{ marginBottom: 10 }}>No fees. Just gas.</p>
       <p className="TextBlock">
         10,000 star systems, each with a story to tell. Who inhabits these
         strange and varied environments? What riches and mysteries do they hold?
